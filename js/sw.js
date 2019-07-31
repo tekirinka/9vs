15d70f9ae4f89a9be02cd0896c0f10e5e7292a23
Your Name <you@example.com>
2019-07-31 13:24:16 +0300

    upd

--- js/sw.js
+++ js/sw.js
@@ -1,30 +1,20 @@
 wburl = 'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js';
 importScripts(wburl);
 
-workbox.precaching.precacheAndRoute([
-  '/',
-  wburl
-]);
+workbox.precaching.precacheAndRoute(['/', wburl]);
 
-workbox.routing.registerRoute(
-  /\.(js|css)$/,
-  new workbox.strategies.StaleWhileRevalidate({
+workbox.routing.registerRoute(/\.(js|css)$/, new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'static',
-  }),
-);
+}), );
 
-workbox.routing.registerRoute(
-  /\.(ico|png|jpeg|jpg)$/,
-  new workbox.strategies.CacheFirst({
+workbox.routing.registerRoute(/\.(ico|png|jpeg|jpg)$/, new workbox.strategies.CacheFirst({
     cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
+    plugins: [new workbox.expiration.Plugin({
         maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-      }),
-      new workbox.cacheableResponse.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+        // 30 days
+    }), new workbox.cacheableResponse.Plugin({
         statuses: [0, 200],
-      }),
-    ],
-  }),
-); // cache images for 30 days
+    }), ],
+}), );
+// cache images for 30 days
