e171455f2219f03321422d0d26bde34978f6524c
Your Name <you@example.com>
2019-08-30 01:21:14 +0300

    hw load bugs added

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 (global => {
   global['app'] = {
+    data: {
+      apikey: '5d4da79558a35b31adeba65f',
+      url: 'https://howork-3bce.restdb.io/rest',
+      template: document.querySelector('template.hw').innerHTML
+    },
     routes: {
-      group1: { rendered: _ => {} },
-      group2: { rendered: _ => {} }
+      hw: {
+        rendered: async (hw, group) => {
+        if (!(hw in global.app.data)) return;
+        let loading = document.querySelector(`body > .loading`);
+        loading.style.opacity = '1';
+        global.app.data[hw] = await fetch(`${global.app.data.url}/${group}`, {
+          headers: {
+            'x-apikey': global.app.data.apikey
+          }
+        });
+        let element = document.querySelector(`${location.hash} .list`);
+        element.innerHTML = global.app.data[hw].map(
+          item => global.app.data.template
+            .replace('TITLE', item.title)
+            .replace('CONTENT', item.content)
+        );
+        
+        loading.style.opacity = '0';
+        }
+      },
+      group1: {p
+        rendered: _ => global.app.routes.hw('hw1', 'group1')
+      }
+      group2: {
+        rendered: _ => global.app.routes.hw('hw2', 'group2')
+      }
     },
     default: 'home',
     hashChange: _ => {
