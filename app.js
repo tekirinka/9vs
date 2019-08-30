0d2563df8e3eff7e758a8df3a767084f294f7b2a
asd <asd@gmail.com>
2019-08-30 15:00:09 +0300

    upd

--- app.js
+++ app.js
@@ -1,49 +1,52 @@
-(global=>{
-    global['app'] = {
-        data: {
-            apikey: '5d4da79558a35b31adeba65f',
-            url: 'https://howork-3bce.restdb.io/rest',
-            template: document.querySelector('template.hw').innerHTML
-        },
-        routes: {
-            hw: {
-                rendered: async(init,hw,group)=>{
-                    if (hw in global.app.data)
-                        return;
-                    if (!init)
-                        document.body.classList.add('loading');
-                    global.app.data[hw] = await fetch(`${global.app.data.url}/${group}`,{headers:{'x-apikey':global.app.data.apikey}}).then(x=>x.json());
-                    let element = document.querySelector(`${location.hash} .list`);
-                    element.innerHTML = global.app.data[hw].map(item=>global.app.data.template.replace('TITLE', item.title).replace('CONTENT', item.content));
-                    if (!init)
-                        document.body.classList.remove('loading');
-                }
-            },
-            group1: {
-                rendered: init=>global.app.routes.hw.rendered(init, 'hw1', 'group1')
-            },
-            group2: {
-                rendered: init=>global.app.routes.hw.rendered(init, 'hw2', 'group2')
-            }
-        },
-        default: 'home',
-        hashChange: init=>{
-            app.route = app.routes[location.hash.slice(1)];
-            if (app.route) {
-                app.route.rendered(init);
-            }
+(global => {
+  global["app"] = {
+    data: {
+      apikey: "5d4da79558a35b31adeba65f",
+      url: "https://howork-3bce.restdb.io/rest",
+      template: document.querySelector("template.hw").innerHTML
+    },
+    routes: {
+      hw: {
+        rendered: async (init, hw, group) => {
+          if (hw in global.app.data) return;
+          if (!init) document.body.classList.add("loading");
+          global.app.data[hw] = await fetch(`${global.app.data.url}/${group}`, {
+            headers: { "x-apikey": global.app.data.apikey }
+          }).then(x => x.json());
+          let element = document.querySelector(`${location.hash} .list`);
+          element.innerHTML = global.app.data[hw]
+            .map(item =>
+              global.app.data.template
+                .replace("TITLE", item.title)
+                .replace("CONTENT", item.content)
+            )
+            .join("");
+          if (!init) document.body.classList.remove("loading");
         }
-        ,
-        init: _=>{
-            global.addEventListener('hashchange', _=>app.hashChange());
-            if (window.location.hash) {
-                app.hashChange(true);
-            } else {
-                location.hash = app.default;
-            }
-        }
-    };
-}
-)(window);
+      },
+      group1: {
+        rendered: init => global.app.routes.hw.rendered(init, "hw1", "group1")
+      },
+      group2: {
+        rendered: init => global.app.routes.hw.rendered(init, "hw2", "group2")
+      }
+    },
+    default: "home",
+    hashChange: init => {
+      app.route = app.routes[location.hash.slice(1)];
+      if (app.route) {
+        app.route.rendered(init);
+      }
+    },
+    init: _ => {
+      global.addEventListener("hashchange", _ => app.hashChange());
+      if (window.location.hash) {
+        app.hashChange(true);
+      } else {
+        location.hash = app.default;
+      }
+    }
+  };
+})(window);
 
 app.init();
