f40202c56bd33347e17ffd67f6c96396e63dfa2e
asd <asd@gmail.com>
2019-08-30 14:17:36 +0300

    upd

--- app.js
+++ app.js
@@ -8,29 +8,29 @@
     routes: {
       hw: {
         rendered: async (hw, group) => {
-        if (!(hw in global.app.data)) return;
-        let loading = document.querySelector(`body > .loading`);
-        loading.style.opacity = '1';
-        global.app.data[hw] = await fetch(`${global.app.data.url}/${group}`, {
-          headers: {
-            'x-apikey': global.app.data.apikey
-          }
-        });
-        let element = document.querySelector(`${location.hash} .list`);
-        element.innerHTML = global.app.data[hw].map(
-          item => global.app.data.template
-            .replace('TITLE', item.title)
-            .replace('CONTENT', item.content)
-        );
-        
-        loading.style.opacity = '0';
+          if (!(hw in global.app.data)) return;
+          let loading = document.querySelector(`body > .loading`);
+          loading.style.opacity = '1';
+          global.app.data[hw] = await fetch(`${global.app.data.url}/${group}`, {
+            headers: {
+              'x-apikey': global.app.data.apikey
+            }
+          });
+          let element = document.querySelector(`${location.hash} .list`);
+          element.innerHTML = global.app.data[hw].map(item =>
+            global.app.data.template
+              .replace('TITLE', item.title)
+              .replace('CONTENT', item.content)
+          );
+
+          loading.style.opacity = '0';
         }
       },
-      group1: {p
-        rendered: _ => global.app.routes.hw('hw1', 'group1')
-      }
+      group1: {
+        rendered: _ => global.app.routes.hw.rendered('hw1', 'group1')
+      },
       group2: {
-        rendered: _ => global.app.routes.hw('hw2', 'group2')
+        rendered: _ => global.app.routes.hw.rendered('hw2', 'group2')
       }
     },
     default: 'home',
