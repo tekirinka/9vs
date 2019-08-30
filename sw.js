3b456ee01f89ade97e89407ebc4fa6e2117a487c
asd <asd@gmail.com>
2019-08-30 22:08:36 +0300

    upd

--- sw.js
+++ sw.js
@@ -1,36 +1,36 @@
-let version = 'main';
+let version = "main";
 let cacheFirst = [
-  '/',
-  '/sw-register.js',
-  '/app.js',
-  '/mf.json',
-  ...['icon-192.png', 'icon.png', 'icon.svg', 'loading.svg'].map(
+  "/",
+  "/sw-register.js",
+  "/app.js",
+  "/mf.json",
+  ...["icon-192.png", "icon.png", "icon.svg", "loading.svg"].map(
     x => `/assets/${x}`
   ),
-  'https://unpkg.com/picnic@6.5.0/picnic.min.css'
+  "https://unpkg.com/picnic@6.5.0/picnic.min.css"
 ];
-let staleWhileRevalidate = ['/app.css'];
+let staleWhileRevalidate = ["/app.css"];
 
-this.addEventListener('install', evt => {
+this.addEventListener("install", evt => {
   self.skipWaiting();
 
   evt.waitUntil(
     caches.open(version).then(cache => {
-      console.time('precaching');
+      console.time("precaching");
       cache.addAll([...cacheFirst, ...staleWhileRevalidate]);
-      console.timeEnd('precaching');
+      console.timeEnd("precaching");
     })
   );
 });
 
-this.addEventListener('activate', async evt => {
+this.addEventListener("activate", async evt => {
   let keys = await caches.keys();
 
   for (key of keys) {
     if (key != version) {
       console.log(
-        'activate',
-        'background:#209CEE;color:white;',
+        "activate",
+        "background:#209CEE;color:white;",
         `delete ${key}`
       );
       caches.delete(key);
@@ -38,30 +38,30 @@ this.addEventListener('activate', async evt => {
   }
 });
 
-this.addEventListener('fetch', async evt => {
-  console.group('fetch');
+this.addEventListener("fetch", async evt => {
+  console.group("fetch");
 
   let url = new URL(evt.request.url);
   console.dir({ url });
   let req = evt.request;
 
-  console.time('cache open');
+  console.time("cache open");
   let cache = caches.open(version);
-  console.timeEnd('cache open');
+  console.timeEnd("cache open");
 
   if (url.pathname in cacheFirst) {
-    console.log('cache first');
+    console.log("cache first");
     evt.respondWith(caches.match(req));
   } else if (url.pathname in staleWhileRevalidate) {
-    console.log('stale while revalidate');
+    console.log("stale while revalidate");
     evt.respondWith(caches.match(evt.request));
     try {
       let resp = await fetch(evt.request);
       cache.put(evt.request, resp);
-      console.log('cache update OK');
+      console.log("cache update OK");
     } catch {}
   } else {
-    console.log('network first');
+    console.log("network first");
     evt.respondWith(
       fetch(req)
         .then(resp => {
@@ -71,5 +71,5 @@ this.addEventListener('fetch', async evt => {
         .catch(_ => caches.match(req))
     );
   }
-  console.groupEnd('fetch');
+  console.groupEnd("fetch");
 });
