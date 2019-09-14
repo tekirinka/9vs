ca0022724cbcdf7160ff9b0a2532a2c15a9a0538
asd <asd@c.vb>
2019-09-14 19:00:57 +0300

    use web worker

--- app.js
+++ app.js
@@ -23,12 +23,10 @@ app = {
   },
   data: {
     default: "#home",
-    apikey: "5d4da79558a35b31adeba65f",
-    url: "https://howork-3bce.restdb.io/rest",
     template: document.querySelector("template.hw").content,
     groups: [
-      { page: "#group1", db: "group1" },
-      { page: "#group2", db: "group2" }
+      { page: "#group2", db: "group2" },
+      { page: "#group1", db: "group1" }
     ],
     hw: [],
     months: "января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря".split(
@@ -41,39 +39,49 @@ app = {
   fetch: _ => {
     app.data.groups.map(async group => {
       let { page, db } = group;
-      new Promise(async (ok, err) => {
+      new Promise((ok, err) => {
         if (!app.data.hw[db]) {
-          app.data.hw[db] = await fetch(`${app.data.url}/${db}`, {
-            headers: { "x-apikey": app.data.apikey }
-          }).then(x => x.json(), err);
+          let worker = new Worker("fetch.js");
+          worker.onmessage = evt => {
+            if (evt.data.type == "err") {
+              err(evt.data.err);
+            }
+            app.data.hw[db] = evt.data.json;
+            ok(app.data.hw[db]);
+          };
+          worker.postMessage(db);
+        } else {
+          ok(app.data.hw[db]);
         }
-        ok(app.data.hw[db]);
-      }).then(hw => {
-        let dateNow = new Date(Date.now());
-        dateNow.setHours(0, 0, 0, 0);
-        hw = hw.filter(item => new Date(item.date) >= dateNow);
-        hw = hw.sort((a, b) => new Date(a.date) - new Date(b.date));
-        const element = document.querySelector(`${page} .list`);
-        element.innerHTML = "";
-        for (item of hw) {
-          const template = document.importNode(app.data.template, true);
-          const date = new Date(item.date);
-          const dateText = `${date.getDate()} ${
-            app.data.months[date.getMonth()]
-          } ${date.getFullYear()} года, ${
-            date.getDate() == dateNow.getDate()
-              ? "сегодня"
-              : date.getDate() == dateNow.getDate() + 1
-              ? "завтра"
-              : app.data.weekdays[date.getDay() - 1]
-          }`;
-          template.querySelector(
-            ".title"
-          ).innerHTML = `${item.title} (${dateText})`;
-          template.querySelector(".content").innerHTML = item.content;
-          element.appendChild(template);
-        }
-      });
+      })
+        .then(hw => {
+          const dateNow = new Date(Date.now());
+          dateNow.setHours(0, 0, 0, 0);
+          hw = hw
+            .filter(item => new Date(item.date) >= dateNow)
+            .sort((a, b) => new Date(a.date) - new Date(b.date));
+          const element = document.querySelector(`${page} .list`);
+          element.innerHTML = "";
+          for (item of hw) {
+            const template = document.importNode(app.data.template, true);
+            const date = new Date(item.date);
+            const dateText = `${date.getDate()} ${
+              app.data.months[date.getMonth()]
+            } ${date.getFullYear()} года, ${
+              date.getDate() == dateNow.getDate()
+                ? "сегодня"
+                : date.getDate() == dateNow.getDate() + 1
+                ? "завтра"
+                : app.data.weekdays[date.getDay() - 1]
+            }`;
+            template.querySelector(
+              ".title"
+            ).innerHTML = `${item.title} (${dateText})`;
+            template.querySelector(".content").innerHTML = item.content;
+            element.appendChild(template);
+          }
+        })
+        .catch(console.err);
     });
   },
   routes: {}
