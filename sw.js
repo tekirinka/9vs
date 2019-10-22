f5d0df7161c1c78faad438a4e7302abfe1c490fe
asd <asd@gmail.com>
2019-10-22 17:12:57 +0300

    improvements

--- sw.js
+++ sw.js
@@ -1,47 +1,36 @@
 const CACHE = "path";
+let CACHE_PROMISE;
+async function cache() {
+  if (!CACHE_PROMISE) CACHE_PROMISE = await caches.open(CACHE);
+  return CACHE_PROMISE;
+}
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches
-      .keys()
-      .then(x => x.map(key => key != CACHE && caches.delete(key)))
-      .then(_ => {
-        caches
-          .open(CACHE)
-          .then(cache =>
-            cache
-              .addAll([
-                "/",
-                "/app.js",
-                "/app.css",
-                "/sw-register.js",
-                "/sw.js",
-                "/fetch.js",
-                "/mf.json",
-                "/assets/icon-192.png",
-                "/assets/icon.png",
-                "/assets/icon.svg",
-                "/assets/loading.svg",
-                "/vendor/picnic.min.css"
-              ])
-              .then(self.skipWaiting)
-          );
-      })
-  );
+self.addEventListener("install", async event => {
+  let c = await cache();
+  await c.addAll([
+    "/",
+    "/app.js",
+    "/app.css",
+    "/sw-register.js",
+    "/sw.js",
+    "/fetch.js",
+    "/mf.json",
+    "/assets/icon-192.png",
+    "/assets/icon.png",
+    "/assets/icon.svg",
+    "/assets/loading.svg",
+    "/vendor/picnic.min.css"
+  ]);
+  evt.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", async evt => {
-  evt.waitUntil(
-    caches
-      .keys()
-      .then(x => x.map(key => key != CACHE && caches.delete(key)))
-      .then(_ => {
-        self.clients.claim();
-      })
-  );
+  let keys = await caches.keys();
+  keys.map(key => key != CACHE && caches.delete(key));
+  evt.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", evt => {
   if (evt.request.url in ["/home", "/group2", "/shedule"])
     evt.request.url = "/";
   evt.respondWith(networkOrCache(evt.request).catch(() => useFallback()));
@@ -61,19 +50,18 @@ async function networkOrCache(req) {
   }
 }
 
-function useFallback() {
-  return caches.open(CACHE).then(cache => cache.match(new Request("/")));
+async function useFallback() {
+  let c = await cache();
+  return c.match(new Request("/"));
 }
 
-function fromCache(req) {
-  return caches
-    .open(CACHE)
-    .then(cache =>
-      cache.match(req).then(res => res || Promise.reject("no-match"))
-    );
+async function fromCache(req) {
+  let c = await cache();
+  return (await c.match(req)) || Promise.reject("no cache match");
 }
 
 async function cacheRes(req, res) {
-  await caches.open(CACHE).then(cache => cache.put(req, res.clone()));
+  let c = await cache();
+  c.put(req, res.clone());
   return res;
 }
